e0033a4e95966735a5f03e25f0a3c3628acdf579
sampletext <sampletext>
2017-11-02 01:56:25 +0700

    Sending

--- app/ipc/ipc_api.js
+++ app/ipc/ipc_api.js
@@ -36,13 +36,20 @@ electron.ipcMain.on('web3-req-channel', (event, arg) => {
         event.sender.send('web3-res-channel', resultBody);
     }, (error) => {
         logger.error('Unable to process process request: ' + error);
-        let resultBody = {id: arg.id, result: null, error: error};
+        let resultError = error;
+        if (error.message) {
+            resultError = error.message;
+            if (error.stack) {
+                logger.silly(error.stack);
+            }
+        }
+        let resultBody = {id: arg.id, result: null, error: resultError};
         event.sender.send('web3-res-channel', resultBody);
     });
 });
 
 function isSyncing() {
-    return new Promise((resolve)=> {
+    return new Promise((resolve) => {
         let web3 = web3service.getWeb3();
 
         if (!web3) {
@@ -105,6 +112,47 @@ function getBlockNumber() {
     });
 }
 
+function lockAccount(account) {
+    return new Promise((resolve, reject) => {
+        let web3 = web3service.getWeb3();
+
+        if (!web3) {
+            reject('Web3 is null');
+        }
+
+        web3.personal.lockAccount(account, function (err, lockAccount) {
+            if (err) {
+                reject(err);
+            } else {
+                return resolve(lockAccount);
+            }
+        });
+    });
+
+}
+
+function unlockAccount(unlockData) {
+    return new Promise((resolve, reject) => {
+        let web3 = web3service.getWeb3();
+
+        if (!web3) {
+            reject('Web3 is null');
+        }
+        if (!unlockData || (!unlockData.account || !unlockData.password)) {
+            reject('Invalid params')
+        }
+
+        web3.personal.unlockAccount(unlockData.account, unlockData.password, function (err, lockAccount) {
+            if (err) {
+                reject(err);
+            } else {
+                return resolve(lockAccount);
+            }
+        });
+    });
+
+}
+
 function getAccounts() {
     return new Promise((resolve) => {
         let web3 = web3service.getWeb3();
@@ -124,7 +172,7 @@ function getAccounts() {
 }
 
 function getBalance(address) {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         let web3 = web3service.getWeb3();
 
         if (!web3) {
@@ -142,8 +190,27 @@ function getBalance(address) {
     });
 }
 
+function sendTransaction(transactionData) {
+    return new Promise((resolve, reject) => {
+        let web3 = web3service.getWeb3();
+
+        if (!web3) {
+            reject('web3 is null')
+        }
+
+        //todo validate
+        web3.eth.sendTransaction(transactionData, function (err, receipt) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(receipt);
+            }
+        });
+    });
+}
+
 function newAccount(password) {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         let web3 = web3service.getWeb3();
 
         if (!web3) {
@@ -151,14 +218,14 @@ function newAccount(password) {
         }
 
 
-/*        web3.eth.personal.newAccount(password, function (err, balance) {
-            if (err) {
-                reject(err);
-            } else {
-                let result = balance.toString(10);
-                resolve(result);
-            }
-        });*/
+        /*        web3.eth.personal.newAccount(password, function (err, balance) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        let result = balance.toString(10);
+                        resolve(result);
+                    }
+                });*/
     });
 }
 
@@ -187,7 +254,7 @@ function requestDecoder(data) {
                 });
                 break;
             case 'get_balance':
-                if(!data.params) {
+                if (!data.params) {
                     reject('No params')
                 }
                 getBalance(data.params).then((result) => {
@@ -195,10 +262,34 @@ function requestDecoder(data) {
                 }, rej => reject(rej));
                 break;
             case 'new_account':
-                if(!data.params) {
+                if (!data.params) {
                     reject('No params')
                 }
                 newAccount(data.params).then((result) => {
+                    resolve(result);
+                }, rej => reject(rej));
+                break;
+            case 'send_transaction':
+                if (!data.params && (!data.params.from && !data.params.to && !data.params.from)) {
+                    reject('No params')
+                }
+                sendTransaction(data.params).then((result) => {
+                    resolve(result);
+                }, rej => reject(rej));
+                break;
+            case 'lock_account':
+                if (!data.params) {
+                    reject('No params')
+                }
+                lockAccount(data.params).then((result) => {
+                    resolve(result);
+                }, rej => reject(rej));
+                break;
+            case 'unlock_account':
+                if (!data.params) {
+                    reject('No params')
+                }
+                unlockAccount(data.params).then((result) => {
                     resolve(result);
                 }, rej => reject(rej));
                 break;
